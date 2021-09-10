df89b6c91f59c523566862954f1840c59800e317
Egemen <egemenkar@gmail.com>
2021-09-10 16:39:21 +0300

    Added ClientSide

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const FoodModel = require("./models/Food");
 
@@ -8,13 +9,18 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("", {
     useNewUrlParser: true,
 });
 
-app.get("/", async (req, res) => {
-    const food = new FoodModel({ foodName: "Banana", daysSinceIAte: 5 });
+app.post("/insert", async (req, res) => {
+
+    const foodName = req.body.foodName;
+    const days = req.body.days;
+
+    const food = new FoodModel({ foodName: foodName, daysSinceIAte: days });
 
     try {
         await food.save();
@@ -23,6 +29,18 @@ app.get("/", async (req, res) => {
         console.log(err);
     }
 });
+
+app.get("/read", async (req, res) => {
+
+    FoodModel.find({}, (err, result)=> {
+        if (err) {
+            res.send(err);
+        }
+
+        res.send(result);
+    })
+});
+
 
 app.listen(3001, () => {
     console.log("Server running on port 3001...");
