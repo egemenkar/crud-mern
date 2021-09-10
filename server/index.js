f1da1b348d563b95a653a93f6a41d63327e4c037
Egemen <egemenkar@gmail.com>
2021-09-10 14:17:54 +0300

    Server Added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const FoodModel = require("./models/Food");
+
 const app = express();
+
 
 
 app.use(express.json());
 
 mongoose.connect("", {
     useNewUrlParser: true,
+});
+
+app.get("/", async (req, res) => {
+    const food = new FoodModel({ foodName: "Banana", daysSinceIAte: 5 });
+
+    try {
+        await food.save();
+        res.send("insterted data");
+    } catch(err) {
+        console.log(err);
+    }
 });
 
 app.listen(3001, () => {
